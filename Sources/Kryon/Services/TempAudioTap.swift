224f0af600f792b32930a65852a00cfd5e47cd63
ayushmaninbox <146472718+ayushmaninbox@users.noreply.github.com>
2026-06-11 16:49:19 +0530

    clean up temporary DMG mount folders on uninstaller abort

---
+++ Sources/Kryon/Services/TempAudioTap.swift
@@ -0,0 +1,9 @@
+// Experimental audio tap prototype
+import Foundation
+import CoreAudio
+
+struct TempAudioTap {
+    static func testAudioTap() {
+        print("Testing legacy audio tap callback...")
+    }
+}
